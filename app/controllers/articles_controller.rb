6180c070ede35d8055d4586d0ae1c78a1db9d2c2
Haechal Abdillah <haechalas@gmail.com>
2020-10-16 16:19:27 +0700

    Add some link and DRY refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit , :update , :destroy] 
+    
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -9,36 +12,46 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
-
     end
 
     def edit
-        @article = Article.find(params[:id])
-
     end
 
     
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
     if  @article.save
         flash[:notice] = "Article created successfully"
         redirect_to article_path(@article)
     else
         render 'new'
     end
-end
 
-def update
-    @article = Article.find(params[:id])
-if  @article.update(params.require(:article).permit(:title, :description))
-    flash[:notice] = "Article was successfully updated"
-    redirect_to @article
-else
-    render 'edit'
+    def update
+    if  @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to @article
+    else
+        render 'edit'
+            end
         end
     end
+    
+    def destroy
+        @article.destroy
+        redirect_to @article
+    end
+        
+    private 
 
+    def set_article
+        @article = Article.find(params[:id])
+    end
 
-
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
 end
+
